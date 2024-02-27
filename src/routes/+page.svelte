12984584ee4cbede4507a9a8ac984f5a26df905e
farxa <r.abualhassan@gmail.com>
2024-02-27 19:38:01 +0100

    Refactor page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,7 @@
-<section class="flex flex-col items-center justify-center mx-[32px] md:mx-[80px] gap-3">
-	<div>
-		<h1 class="text-[24px] md:text-[30px] lg:text-[36px] font-normal text-center text-[#111928]">
-			We didn't reinvent the wheel
-		</h1>
-		<h2
-			class="text-[12px] md:text-[16px] lg:text-[18px] font-normal text-center text-[#6B7280] mx-0 lg:mx-38"
-		>
-			We are strategists, designers and developers. Innovators and problem solvers. Small enough to
-			be simple and quick, but big enough to deliver the scope you want at the pace you need.
-		</h2>
-	</div>
-	<div class="grid gap-2">
-		<div class="grid grid-cols-4 gap-2">
-			<img src="/row1image1.png" alt="row1image1" class="col-span-2 w-full h-full object-cover" />
-			<img src="/row1image2.png" alt="row1image2" class="col-span-1 w-full h-full object-cover" />
-			<img src="/row1image3.png" alt="row1image3" class="col-span-1 w-full h-full object-cover" />
-		</div>
-		<div class="grid grid-cols-4 gap-2">
-			<img src="/row2image1.png" alt="row2image1" class="col-span-1 w-full h-full object-cover" />
-			<img src="/row2image2.png" alt="row2image2" class="col-span-2 w-full h-full object-cover" />
-			<img src="/row2image3.png" alt="row2image3" class="col-span-1 w-full h-full object-cover" />
-		</div>
-	</div>
-</section>
+<script lang="ts">
+	import Introduction from '../components/Introduction.svelte';
+	import Pricing from '../components/Pricing.svelte';
+</script>
+
+<Introduction />
+<Pricing />
