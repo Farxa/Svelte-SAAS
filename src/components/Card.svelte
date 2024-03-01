55cd0f11819261e24a5a30ed471fc8855363f5b4
farxa <r.abualhassan@gmail.com>
2024-03-01 19:24:31 +0100

    show cached data while updated data is being fetched and reflect loading in the text behavour. disable links and buttons during loading

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import SkeletonLoader from './SkeletonLoader.svelte';
 	export let name: string;
 	export let formattedAmount: string;
 	export let mostPopular: boolean;
@@ -19,46 +18,47 @@
 		mostPopular ? 'border-[#1C64F2]' : ''
 	} shadow-md w-full h-fit gap-3`}
 >
-	{#if isLoading}
-		{#if mostPopular}
-			<SkeletonLoader width="w-1/2" height="h-4" marginBottom="mb-2" />
-		{/if}
-		<SkeletonLoader width="w-3/4" height="h-8" marginBottom="mb-4" />
-		<SkeletonLoader width="w-2/3" height="h-6" marginBottom="mb-4" />
-		<SkeletonLoader width="w-1/2" height="h-10" marginBottom="mb-4" />
-		<SkeletonLoader width="w-full" height="h-10" marginBottom="mb-6" />
-		<SkeletonLoader width="w-full" height="h-4" marginBottom="mb-2" />
-		<SkeletonLoader width="w-full" height="h-4" marginBottom="mb-2" />
-		<SkeletonLoader width="w-full" height="h-4" marginBottom="mb-2" />
-	{:else}
-		{#if mostPopular}
-			<div class="mb-2 text-sm text-[#1E429F] bg-blue-50 px-3 py-1 rounded-md">Most popular</div>
-		{/if}
-
-		<h2 class="text-2xl font-semibold text-gray-800 mb-4">{name}</h2>
-		<p class="text-4xl font-bold text-gray-900 mb-4">{formattedAmount}</p>
-		<button on:click={toggleInterval} class="text-[#1C64F2] text-sm mb-4">
-			<slot name="buttonText">Default Button Text</slot>
-		</button>
-
-		<button
-			class={`text-white font-bold py-2 px-4 mb-6 w-full rounded-md ${
-				mostPopular ? 'bg-[#1C64F2]' : 'bg-black'
-			}`}
-		>
-			Get started
-		</button>
-
-		<ul class="w-full text-gray-600 text-left">
-			{#each features as feature}
-				<li class="flex items-center mb-2">
-					<span class="w-5 h-5 inline-block mr-4">
-						<img src={feature.img} alt={feature.text} class="w-6 h-6" />
-					</span>
-					{feature.text}
-				</li>
-				<div class="i-unicons:home w-1em h-1em" style="color: black;" />
-			{/each}
-		</ul>
+	{#if mostPopular}
+		<div class="mb-2 text-sm text-[#1E429F] bg-blue-50 px-3 py-1 rounded-md">Most popular</div>
 	{/if}
+
+	<h2
+		class="text-2xl font-semibold {isLoading
+			? 'text-gray-300 animate-pulse'
+			: 'text-gray-900'} mb-4"
+	>
+		{name}
+	</h2>
+	<p class="text-4xl font-bold {isLoading ? 'text-gray-300 animate-pulse' : 'text-gray-900'}  mb-4">
+		{formattedAmount}
+	</p>
+	<button
+		on:click={toggleInterval}
+		disabled={isLoading}
+		class="{isLoading ? 'text-gray-300 cursor-not-allowed' : 'text-[#1C64F2]'} text-sm mb-4"
+	>
+		<slot name="buttonText">Default Button Text</slot>
+	</button>
+
+	<button
+		on:click={toggleInterval}
+		disabled={isLoading}
+		class="text-white font-bold py-2 px-4 mb-6 w-full rounded-md {mostPopular
+			? 'bg-[#1C64F2]'
+			: 'bg-black'} {isLoading ? 'bg-gray-400 cursor-not-allowed' : ''}"
+	>
+		Get started
+	</button>
+
+	<ul class="w-full text-gray-600 text-left">
+		{#each features as feature}
+			<li class="flex items-center mb-2">
+				<span class="w-5 h-5 inline-block mr-4">
+					<img src={feature.img} alt={feature.text} class="w-6 h-6" />
+				</span>
+				{feature.text}
+			</li>
+			<div class="i-unicons:home w-1em h-1em" style="color: black;" />
+		{/each}
+	</ul>
 </div>
