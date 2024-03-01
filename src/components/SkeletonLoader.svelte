51a893b9892d043f5f4d503e95bf8bdff446d0f7
farxa <r.abualhassan@gmail.com>
2024-03-01 16:32:01 +0100

    skeleton loading

---
+++ src/components/SkeletonLoader.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let width = 'w-full';
+	export let height = 'h-4';
+	export let marginBottom = 'mb-2';
+</script>
+
+<div
+	class={`bg-gray-200 rounded ${width} ${height} ${marginBottom}`}
+	style="animation: pulse 2s infinite;"
+/>
