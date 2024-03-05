607b017b193497e1a958e941a68ba840d018e585
farxa <r.abualhassan@gmail.com>
2024-03-05 20:32:43 +0100

    extract util to validate email to its own utils file and use it in newsletter component

--- src/components/Newsletter.svelte
+++ src/components/Newsletter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
+	import { isValidEmail } from '$lib/utils';
 	import IconifyIcon from '@iconify/svelte';
 	import PageWrapper from './PageWrapper.svelte';
 	import Heading from './Heading.svelte';
@@ -7,11 +8,6 @@
 	let email: string = '';
 	let emailValidationMessage = writable('');
 	let successMessage = writable('');
-
-	function isValidEmail(email: string): boolean {
-		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		return regex.test(email);
-	}
 
 	async function submitEmail(): Promise<void> {
 		emailValidationMessage.set('');
@@ -54,12 +50,8 @@
 		description="Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up with your email."
 	/>
 	<form on:submit={handleSubmit} class="p-4 w-full max-w-lg mx-auto" novalidate>
-		<!-- <div class="p-4 max-w-lg mx-auto"> 
-		<div class="flex items-center">
-			<div class="flex-1">-->
 		<div class="flex flex-col md:flex-row items-center space-y-4 sm:space-y-0">
 			<div class="relative flex-1">
-				<!-- <span class="absolute py-4 px-7 pl-5"> -->
 				<span class="absolute inset-y-0 left-0 flex items-center pl-3">
 					<IconifyIcon icon="mdi:email-outline" class="text-gray-500" />
 				</span>
@@ -88,6 +80,5 @@
 			We care about the protection of your data. Read our
 			<a href="/privacy-policy" class="text-gray-700 underline">Privacy Policy</a>.
 		</div>
-		<!-- </div> -->
 	</form>
 </PageWrapper>
