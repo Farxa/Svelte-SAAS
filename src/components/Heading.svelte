148a56ae01e3938ab8aabdd72c10467f7598d4c6
farxa <r.abualhassan@gmail.com>
2024-03-05 22:55:46 +0100

    Add isXl prop to Heading component

--- src/components/Heading.svelte
+++ src/components/Heading.svelte
@@ -2,21 +2,26 @@
 	export let title: string = 'Default Title';
 	export let description: string = 'Default description.';
 	export let itemsAlignment: string = 'items-center';
+	export let isXl: boolean = false;
 </script>
 
 <div class="flex flex-col {itemsAlignment} gap-3 mb-10">
 	<h1
-		class="text-[26px] md:text-[30px] lg:text-[36px] {itemsAlignment === 'items-start'
+		class=" {isXl
+			? 'text-[30px] md:text-[30px] lg:text-[36px]'
+			: 'text-[26px] md:text-[30px] lg:text-[36px]'} {itemsAlignment === 'items-start'
 			? ' text-start'
 			: 'text-center'} text-[#111928]"
 	>
 		{title}
 	</h1>
 	<h2
-		class="text-[14px] md:text-[16px] lg:text-[18px] text-[#6B7280] mx-0 {itemsAlignment ===
+		class="{isXl
+			? 'text-[18px] md:text-[26px] lg:text-[20px]'
+			: 'text-[14px] md:text-[16px] lg:text-[18px]'} text-[#6B7280] mx-0 {itemsAlignment ===
 		'items-start'
 			? ' text-start'
-			: 'text-center lg:mx-38'} "
+			: 'text-center'} "
 	>
 		{description}
 	</h2>
