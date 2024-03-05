65e3e43e1f36cb27f3e843e05f906e97fc852299
farxa <r.abualhassan@gmail.com>
2024-03-05 20:03:51 +0100

    refactor Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
 	import IconifyIcon from '@iconify/svelte';
+	const socialLinks = [
+		{ icon: 'mdi-facebook', label: 'Facebook', href: 'https://facebook.com' },
+		{ icon: 'mdi-twitter', label: 'Twitter', href: 'https://twitter.com' },
+		{ icon: 'mdi-github', label: 'GitHub', href: 'https://github.com' },
+		{ icon: 'mdi-dribbble', label: 'dribbble', href: 'https://dribbble.com' }
+	];
 </script>
 
 <footer class="flex justify-between items-center p-4 mt-24 bg-white">
-	<span class="text-sm text-gray-500"> © 2021 Flowbite, LLC. All rights reserved. </span>
+	<span class="text-sm text-gray-500">© 2021 Flowbite, LLC. All rights reserved.</span>
 	<div class="flex items-center space-x-4">
-		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="Facebook">
-			<IconifyIcon icon="mdi-facebook" />
-		</a>
-		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="Twitter">
-			<IconifyIcon icon="mdi-twitter" />
-		</a>
-		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="GitHub">
-			<IconifyIcon icon="mdi-github" />
-		</a>
-		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="LinkedIn">
-			<IconifyIcon icon="mdi-dribbble" />
-		</a>
+		{#each socialLinks as { icon, label, href }}
+			<a {href} class="text-gray-500 hover:text-gray-900" aria-label={label}>
+				<IconifyIcon {icon} />
+			</a>
+		{/each}
 	</div>
 </footer>
