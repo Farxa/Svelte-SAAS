a5c64f75172552e043fcdd59c6fa273cd9742a47
farxa <r.abualhassan@gmail.com>
2024-03-05 19:56:23 +0100

    Add Footer components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import IconifyIcon from '@iconify/svelte';
+</script>
+
+<footer class="flex justify-between items-center p-4 mt-24 bg-white">
+	<span class="text-sm text-gray-500"> © 2021 Flowbite, LLC. All rights reserved. </span>
+	<div class="flex items-center space-x-4">
+		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="Facebook">
+			<IconifyIcon icon="mdi-facebook" />
+		</a>
+		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="Twitter">
+			<IconifyIcon icon="mdi-twitter" />
+		</a>
+		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="GitHub">
+			<IconifyIcon icon="mdi-github" />
+		</a>
+		<a href="#" class="text-gray-500 hover:text-gray-900" aria-label="LinkedIn">
+			<IconifyIcon icon="mdi-dribbble" />
+		</a>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Introduction from '../components/Introduction.svelte';
 	import Pricing from '../components/Pricing.svelte';
+	import Newsletter from '../components/Newsletter.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="px-[32px] md:px-[80px]">
 	<Introduction />
 	<Pricing />
+	<Newsletter />
+	<Footer />
 </div>
