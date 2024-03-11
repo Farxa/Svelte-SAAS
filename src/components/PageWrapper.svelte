a786f4c140f12cf38b55844aa931ea1002fa84e3
farxa <r.abualhassan@gmail.com>
2024-03-11 12:48:42 +0100

    fix alignment and spacing in pagewrapper

--- src/components/PageWrapper.svelte
+++ src/components/PageWrapper.svelte
@@ -1,3 +1,3 @@
-<section class="flex flex-col items-center justify-center my-[38px] md:mx-[80px] gap-3 pt-24">
+<section class="flex flex-col my-[38px] md:mx-[80px] gap-3 pt-24">
 	<slot />
 </section>
